c6df4abba4f5b06632b77399a3fcfd37a980cc4f
Siddhant Verma <karanspd1289@gmail.com>
2024-03-01 12:37:54 +0530

    Created useState and useEffect hooks

--- src/Person.js
+++ src/Person.js
@@ -1,10 +1,20 @@
+import React, { useState, useEffect } from "react";
+
 function Person(props) {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    setName(props.name);
+    setAge(props.age);
+  }, [props.name, props.age]);
+
   return (
     <div>
       <h1>Hello, I am a person.</h1>
       <div>This Is Person Div</div>
-      <h3>Name: {props.name}</h3>
-      <h3>Age: {props.age}</h3>
+      <h3>Name: {name}</h3>
+      <h3>Age: {age}</h3>
       <h4>Sum = {props.first + props.second}</h4>
     </div>
   );
